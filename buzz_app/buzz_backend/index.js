d0e8485c3a31b9fdfe6bb525bf44208285011e15
Aryan Goyal <Aryan Goyal@ttnpl-5748>
2022-04-07 17:20:34 +0530

    aryan:backend posts

--- buzz_app/buzz_backend/index.js
+++ buzz_app/buzz_backend/index.js
@@ -1,6 +1,17 @@
 const express= require("express");
 const app= express();
+const mongoose= require("mongoose");
+const dotenv= require("dotenv");
+const helmet= require("helmet");
+const morgan= require("morgan");
+const postRoute = require("./routes/posts");
 
-app.get("/",(req, res)=>{res.send("hello")})
+dotenv.config();
+mongoose.connect("mongodb://localhost/student").then(()=>{
+    console.log("connected");
+})
+app.get("/",(req, res)=>{res.send("hello")});
+
+app.use("/api/posts", postRoute);
 
 app.listen(2000,()=>{console.log("running")});
