38af319b596eba2df4e4b811a91b11d3382c7ae4
Anubhav <rajputanubhav71@gmail.com>
2022-04-07 17:58:26 +0530

    added

--- buzz_app/buzz_backend/index.js
+++ buzz_app/buzz_backend/index.js
@@ -1,6 +1,23 @@
 const express= require("express");
 const app= express();
+const mongoose= require("mongoose");
+const dotenv= require("dotenv");
+const helmet= require("helmet");
+const morgan= require("morgan");
+const userRoute= require("./routes/users")
+const authRoute= require("./routes/auth")
 
-app.get("/",(req, res)=>{res.send("hello")})
 
-app.listen(2000,()=>{console.log("running")});
+dotenv.config();
+
+//databse connection
+mongoose.connect("mongodb://localhost/ecomdata")
+.then(()=>{console.log("connected")}).catch((err)=>{console.log(err)})
+
+
+
+
+app.use("/api/users",userRoute)
+app.use("/api/auth",authRoute)
+
+app.listen(4000,()=>{console.log("running")});
