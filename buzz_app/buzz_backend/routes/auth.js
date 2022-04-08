b8e6d83bedd3a9f039bbb5bb8b8e4d9cbb16aefb
Anubhav <rajputanubhav71@gmail.com>
2022-04-08 15:26:49 +0530

    modified

--- buzz_app/buzz_backend/routes/auth.js
+++ buzz_app/buzz_backend/routes/auth.js
@@ -2,8 +2,6 @@ const router= require("express").Router();
 const User= require("../models/User")
 
 router.post("/register", async(req,res)=>{
-
-    
     const newUser= new User({
         username: req.body.username,
         email: req.body.email,
@@ -14,6 +12,19 @@ router.post("/register", async(req,res)=>{
         res.status(200).json(user);
     } catch(err){
         console.log(err)
+    }
+});
+
+router.post("/login",async (req, res)=>{
+    try{
+    const user= await User.findOne({
+        email:req.body.email
+    });
+    !user && res.status(404).send("user not find")
+    user.password===req.body.password?res.send("right"):res.send("wrong password345")
+}
+    catch(e){
+        console.log(e);
     }
 })
 
