cdbaf5bf82e410d08da80b1c9b17e2426ed5f07d
Anubhav <rajputanubhav71@gmail.com>
2022-04-10 18:38:41 +0530

    added files

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+const express= require("express");
+const app = express();
+const mongoose= require("mongoose");
+const dotenv= require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+
+
+ 
+mongoose.connect("mongodb://localhost/studentdata")
+.then(()=>{
+     console.log("connected to mongodb");
+})
+
+//middleware
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+
+
+app.listen(2000,()=>{
+     console.log("backend server is running")
+})
